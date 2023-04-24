2a58025c33f36d44d84a22545ed763cd97018bbe
josh Hsu <58176336+yuenu@users.noreply.github.com>
2023-04-24 15:06:45 +0800

    Create static.yml

--- vite.demo.config.ts
+++ vite.demo.config.ts
@@ -4,4 +4,5 @@ const { defineConfig } = require("vite");
 module.exports = defineConfig({
   root: path.resolve(__dirname, "./demo"),
   publicDir: path.resolve(__dirname, "./demo/public"),
+  base: './'
 });
